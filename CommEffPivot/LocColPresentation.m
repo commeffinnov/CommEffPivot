15467d6199e4cf98e7e4bfdb8dbe8ee688bd9bb5
Cassandra Shi <Cassandra.shih@gmail.com>
2013-10-09 19:57:56 -0400

    temp commit

---
+++ CommEffPivot/LocColPresentation.m
@@ -0,0 +1,13 @@
+//
+//  LocColPresentation.m
+//  CommEffPivot
+//
+//  Created by Cassandra Shi on 10/9/13.
+//  Copyright (c) 2013 Yitong Zhou. All rights reserved.
+//
+
+#import "LocColPresentation.h"
+
+@implementation LocColPresentation
+
+@end
