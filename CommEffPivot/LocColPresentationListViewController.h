84ee3c49923705132c9e9b1e26673b6813beca46
Yitong Zhou <timyitong@gmail.com>
2013-10-19 09:44:43 -0400

    change presentationList to dynamic data push

---
+++ CommEffPivot/LocColPresentationListViewController.h
@@ -0,0 +1,14 @@
+//
+//  LocColPresentationListViewController.h
+//  CommEffPivot
+//
+//  Created by Yitong Zhou on 10/18/13.
+//  Copyright (c) 2013 Yitong Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LocColCourse.h"
+
+@interface LocColPresentationListViewController : UITableViewController
+@property (strong, nonatomic) LocColCourse *course;
+@end
