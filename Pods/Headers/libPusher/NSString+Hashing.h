8c62f29555fac182320dd52d5cae4d21a9477c19
Cassandra Shi <cassandra.shih@gmail.com>
2013-12-04 21:49:37 -0500

    fixed chat

--- Pods/Headers/libPusher/NSString+Hashing.h
+++ Pods/Headers/libPusher/NSString+Hashing.h
@@ -1,1 +1,6 @@
-../../libPusher/Library/NSString+Hashing.h
+#import <Foundation/Foundation.h>
+
+@interface NSString (Hashing)
+- (NSString *)MD5Hash;
+- (NSString *)HMACDigestUsingSecretKey:(id)secretKey;
+@end
